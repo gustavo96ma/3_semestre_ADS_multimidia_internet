d6cfeffe8b0c137893d0886fe38706f747d7a2cd
Gustavo Meneghetti <80553324+gustavo96ma@users.noreply.github.com>
2024-05-06 15:06:59 -0300

    Add files via upload

---
+++ aula1/script.js
@@ -0,0 +1,37 @@
+// Exemplo de código Javascript simples
+console.log('Olá Mundo!');
+
+// Declaração de variáveis
+var nome = "João";
+console.log("Nome: " + nome);
+
+var idade = 25;
+console.log("Idade: " + idade);
+
+var ehEstudante = true;
+console.log("É Estudante: " + ehEstudante);
+
+/*
+    Este é um comentário
+    de múltiplas linhas
+*/
+
+// Operadores
+var x = 10;
+var y = 5;
+console.log("X = " + x, "Y = " + y);
+
+var soma = x + y;
+console.log("Soma = " + soma);
+
+var subtracao = x - y;
+console.log("Subtração = " + subtracao);
+
+var multiplicacao = x * y;
+console.log("Multiplicação = " + multiplicacao);
+
+var divisao = x / y;
+console.log("Divisão = " + divisao);
+
+var modulo = x % y;
+console.log("Módulo = " + modulo);
